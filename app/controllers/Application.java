692c32cd9c45edc5129a72b7e2ca1ebf39c6abe2
Olle Werme <olle.werme@gmail.com>
2013-09-05 03:38:33 +0200

    Add first steps on note CRUD skeleton

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,37 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+
+import models.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+
+  static Form<Note> noteForm = Form.form(Note.class);
+
+  public static Result index() {
+    return ok(views.html.index.render(Note.all(), noteForm));
+  }
+
+  public static Result notes() {
+    return ok(views.html.index.render(Note.all(), noteForm));
+  }
+
+  public static Result newNote() {
+    Form<Note> filledForm = noteForm.bindFromRequest();
+
+    if(filledForm.hasErrors()) {
+      return badRequest(views.html.index.render(Note.all(), filledForm));
+    } else {
+      Note.create(filledForm.get());
+      return redirect(routes.Application.notes());
     }
-  
+  }
+
+  public static Result deleteNote(Long id) {
+    Note.delete(id);
+    return redirect(routes.Application.notes());
+  }
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table note (
+  id                        bigint not null,
+  text                      varchar(255),
+  constraint pk_note primary key (id))
+;
+
+create sequence note_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists note;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists note_seq;
+
