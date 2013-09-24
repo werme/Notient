2db78402bd940256f0b7f2306c32d1a4fdb57079
Olle Werme <olle.werme@gmail.com>
2013-09-24 02:49:10 +0200

    Expand unit tests for notes

--- test/models/NotesTest.java
+++ test/models/NotesTest.java
@@ -1,7 +1,16 @@
-import models.*;
+import com.avaje.ebean.*;
+
+import java.util.*;
+
 import org.junit.*;
-import static org.junit.Assert.*;
+
 import play.test.WithApplication;
+import play.libs.*;
+
+import models.*;
+
+import static org.junit.Assert.*;
+
 import static play.test.Helpers.*;
 
 public class NotesTest extends WithApplication {
@@ -9,13 +18,28 @@ public class NotesTest extends WithApplication {
   @Before
   public void setUp() {
     start(fakeApplication(inMemoryDatabase()));
+    Ebean.save((List) Yaml.load("test-data.yml"));
   }
 
   @Test
-    public void createAndRetrieveUser() {
-      new Note("My note").save();
-      Note myNote = Note.find.where().eq("text", "My note").findUnique();
-      assertNotNull(myNote);
-      assertEquals("My note", myNote.text);
-    }
+  public void createAndRetrieveNote() {
+    new Note("My note").save();
+    Note myNote = Note.find.where().eq("text", "My note").findUnique();
+    assertNotNull(myNote);
+    assertEquals("My note", myNote.text);
+  }
+
+  @Test
+  public void updateNote() {
+    // TODO
+  }
+
+  @Test
+  public void deleteNote() {
+    Note myNote = Note.find.where().eq("text", "Test note").findUnique();
+    Note.delete(myNote.id);
+
+    Note myDeletedNote = Note.find.where().eq("text", "Test note").findUnique();
+    assertNull(myDeletedNote);
+  }
 }
