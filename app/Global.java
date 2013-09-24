c81e8e564f901e0c17195e3503e7066afe195986
Olle Werme <olle.werme@gmail.com>
2013-09-24 02:00:40 +0200

    Add initial data from yaml file

---
+++ app/Global.java
@@ -0,0 +1,19 @@
+import play.*;
+import play.libs.*;
+import java.util.*;
+import com.avaje.ebean.*;
+import models.*;
+
+public class Global extends GlobalSettings {
+
+    public void onStart(Application app) {
+        setupInitialData(app);
+    }
+
+    private void setupInitialData(Application app) {
+        if(Ebean.find(Note.class).findRowCount() == 0) {
+            Logger.info("Setting up initial data...");
+            Ebean.save((List) Yaml.load("initial-data.yml"));
+        }
+    }
+}
