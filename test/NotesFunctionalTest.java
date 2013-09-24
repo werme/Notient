d32075678dd58dc4341f68b65490470a340ab6a8
Olle Werme <olle.werme@gmail.com>
2013-09-24 04:49:44 +0200

    Change to use of ImmutableMap for form data

--- test/NotesFunctionalTest.java
+++ test/NotesFunctionalTest.java
@@ -6,6 +6,7 @@ import play.mvc.*;
 import play.test.WithApplication;
 import static play.test.Helpers.*;
 import models.*;
+import com.google.common.collect.ImmutableMap;
 
 public class NotesFunctionalTest extends WithApplication {
 
@@ -21,12 +22,9 @@ public class NotesFunctionalTest extends WithApplication {
     // Should return bad request if no data is given
     assertThat(status(result)).isEqualTo(BAD_REQUEST);
 
-    Map<String,String> data = new HashMap<String,String>();
-    data.put("text", "My note");
-
     result = callAction(
         controllers.routes.ref.Application.newNote(),
-        fakeRequest().withFormUrlEncodedBody(data)
+        fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("text", "My note"))
     );
 
     // Should return redirect status if successful
