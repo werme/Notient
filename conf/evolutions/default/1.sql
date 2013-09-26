92cdda743968a1d86db8b4c0bf46045cc237ee86
Olle Werme <olle.werme@gmail.com>
2013-09-26 14:40:42 +0200

    Fix evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,3 +1,6 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
 # --- !Ups
 
 create table note (
@@ -7,7 +10,20 @@ create table note (
   constraint pk_note primary key (id))
 ;
 
+create table user (
+  email                     varchar(255) not null,
+  username                  varchar(255),
+  password                  varchar(255),
+  constraint uq_user_1 unique (username),
+  constraint pk_user primary key (email))
+;
+
 create sequence note_seq;
+
+create sequence user_seq;
+
+
+
 
 # --- !Downs
 
@@ -15,6 +31,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists note;
 
+drop table if exists user;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists note_seq;
+
+drop sequence if exists user_seq;
+
