c38b788b0777ed424f2ffb53f59429ebcb309432
Olle Werme <olle.werme@gmail.com>
2013-09-26 03:00:43 +0200

    Take back the automatic sql scripting

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,6 +10,12 @@ create table note (
   constraint pk_note primary key (id))
 ;
 
+create table tag (
+  id                        bigint not null,
+  title                     varchar(255),
+  constraint pk_tag primary key (id))
+;
+
 create table user (
   email                     varchar(255) not null,
   username                  varchar(255),
@@ -18,12 +24,21 @@ create table user (
   constraint pk_user primary key (email))
 ;
 
+create table note_tag (
+  note_id                        bigint not null,
+  tag_id                         bigint not null,
+  constraint pk_note_tag primary key (note_id, tag_id))
+;
+
 create sequence note_seq;
+
+create sequence tag_seq;
 
 create sequence user_seq;
 
+alter table note_tag add constraint fk_note_tag_note_01 foreign key (note_id) references note (id) on delete restrict on update restrict;
 
-
+alter table note_tag add constraint fk_note_tag_tag_02 foreign key (tag_id) references tag (id) on delete restrict on update restrict;
 
 # --- !Downs
 
@@ -31,11 +46,17 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists note;
 
+drop table if exists note_tag;
+
+drop table if exists tag;
+
 drop table if exists user;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists note_seq;
+
+drop sequence if exists tag_seq;
 
 drop sequence if exists user_seq;
 
