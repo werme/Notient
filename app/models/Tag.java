3c03f3d38ce8d5f23a3ce5794a67f0be5074e59b
Olle Werme <olle.werme@gmail.com>
2013-09-27 22:09:24 +0200

    Add unique constraint to tag titles

--- app/models/Tag.java
+++ app/models/Tag.java
@@ -7,6 +7,7 @@ import play.data.format.Formats.*;
 import javax.persistence.*;
 import java.util.regex.PatternSyntaxException;
 
+@Table(uniqueConstraints=@UniqueConstraint(columnNames={"title"}))
 @Entity
 public class Tag extends Model {
 
