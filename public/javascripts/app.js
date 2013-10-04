c7ad5749af5627558341e943dd3dd47e826511a6
Olle Werme <olle.werme@gmail.com>
2013-10-04 13:09:39 +0200

    Add js for quick add menu

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,27 @@
-$('#sub-nav-toggle').click(function() {
-  $('#sub-nav').toggleClass('visible');
-});
+;
+
+(function(){
+
+  e = $('#quick-create-form');
+  e.css({
+    "top": -e.height(),
+    "margin-bottom": -e.height()
+  });
+
+  var toggleMenu = function() {
+    e.css("display", "block");
+
+    if(e.css('top') === '0px') {
+      e.css('top', -e.height());
+      e.css('margin-bottom', -e.height());
+    } else {
+      e.css('top', 0);
+      e.css('margin-bottom', 0);
+    }
+  };
+
+  $('#sub-nav-toggle').click(function() {
+    toggleMenu();
+  });
+
+})();
