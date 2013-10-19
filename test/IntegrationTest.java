62907b1b8268878a677734a4f8b240b8797c9cfd
Marcus Tyren <marcus.tyren@gmail.com>
2013-10-19 17:32:00 +0200

    added tests
    not all will work properly due to lack of id's

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,23 +1,51 @@
-import org.junit.Test;
-import play.libs.F.Callback;
-import play.test.TestBrowser;
+import org.junit.*;
 
-import static org.fest.assertions.Assertions.assertThat;
+import play.test.*;
+import play.libs.F.*;
+
 import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
-
-    /**
-     * Front page test
-     */
+    
     @Test
     public void testFrontPage() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("The first note");
+                
+                assertThat(browser.$("header h1").first().getText()).isEqualTo("notient");
             }
         });
     }
+    @Test
+    public void testLogin() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
 
+                browser.$("#login-button").click();
+                assertThat(browser.$("header h1").first().getText()).isEqualTo("Login");
+                browser.fill("#username").with("pingu1");
+                browser.fill("#password").with("password");
+                browser.$("#submit-button").click();
+                // Checks if routed back to the frontpage
+                assertThat(browser.$("header h1").first().getText()).isEqualTo("notient");
+            }
+        });        
+    }
+    @Test
+    public void testLogout() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+
+                browser.$("#logout-button").click();
+                assertThat(browser.$("header h1").first().getText()).isEqualTo("Login");
+            }
+        });        
+    }
+  
 }
