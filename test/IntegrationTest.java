cba6b28fcc8b612e1753b8dd709c1a8b9bcf3245
Marcus Tyren <marcus.tyren@gmail.com>
2013-10-22 18:32:11 +0200

    refactored login

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -2,6 +2,7 @@ import org.junit.*;
 
 import play.test.*;
 import play.libs.F.*;
+import play.Logger;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
@@ -9,30 +10,36 @@ import static org.fest.assertions.Assertions.*;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
+
+    String baseUrl = "http://localhost";
+    String port = "3333";
+    String myUrl = baseUrl + ":" + port;
+
+    public void login(TestBrowser browser) {
+        browser.goTo(myUrl);
+        browser.$("#login-link").click();
+        browser.$("#username").text("pingu1");
+        browser.$("#password").text("password");
+        browser.$("button", withText("Login")).click();
+    }
     
-    @Before
+    @Test
     public void testLogin() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-
-                browser.$("#login-button").click();
-                assertThat(browser.$("header h1").first().getText()).isEqualTo("Login");
-                browser.fill("#username").with("pingu1");
-                browser.fill("#password").with("password");
-                browser.$("#submit-button").click();
-                // Checks if routed back to the frontpage
-                assertThat(browser.$("header h1").first().getText()).isEqualTo("notient");
+                login(browser);
+                assertThat(browser.$("header h1").first().getText()).isEqualTo("notient");        
+                assertThat(browser.$("nav").first().getText()).contains("Profile");        
             }
         });        
     }
+
 
     @Test
     public void testFrontPage() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                
+                browser.goTo(myUrl);
                 assertThat(browser.$("header h1").first().getText()).isEqualTo("notient");
             }
         });
@@ -41,27 +48,47 @@ public class IntegrationTest {
   public void testLogout() {
     running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                
-                browser.$("#logout-button").click();
-                assertThat(browser.$("header h1").first().getText()).isEqualTo("Login");
+                login(browser);
+                browser.$("#logout-link").click();
+                assertThat(browser.$("h1").first().getText()).isEqualTo("Login");
             }
         });   
   }
+  // @Test
+  // public void testCreateNote() {
+  //   running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+  //           public void invoke(TestBrowser browser) {
+  //               login(browser);
+  //               browser.$("#newnote-button").click();
+  //               browser.fill("#title").with("IntegrationTest");
+  //               browser.fill("#tags").with("tests");
+  //               browser.fill("#content").with("This text is for testing the note creation");
+  //               browser.$("#createnote-button").click();
+
+  //               assertThat(browser.$("new note is found").first().getText()).isEqualTo("IntegratonTest");
+  //           }
+  //       });     
+  // }
   @Test
-  public void testCreateNote() {
+  public void testProfile() {
     running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                
-                browser.$("#newnote-button").click();
-                browser.fill("#title").with("IntegrationTest");
-                browser.fill("#tags").with("tests");
-                browser.fill("#content").with("This text is for testing the note creation");
-                browser.$("#createnote-button").click();
-
-                assertThat(browser.$("new note is found").first().getText()).isEqualTo("IntegratonTest");
+                login(browser);
+                browser.$("#profile-link").click();
+                Logger.debug(browser.url());
+                assertThat(browser.$("div h1").first().getText()).isEqualTo("pingu1");
             }
         });     
   }
+  // @Test
+  // public void testSearch() {
+  //   running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+  //           public void invoke(TestBrowser browser) {
+  //               login(browser);
+                
+  //               browser.$("#profile-link").click();
+  //               assertThat(browser.$("header h1").first().getText()).isEqualTo("pingu1");
+  //           }
+  //       });     
+  // }
 }
